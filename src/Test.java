dd8588e23f30ddf562f24ce6866d6cdc391ddb8c
nguyendinhhoang97 <dinhhoangvcu@gmail.com>
2018-11-19 19:46:05 +0700

    upload

---
+++ src/Test.java
@@ -0,0 +1,17 @@
+public class Test {
+    private static EmailExample emailExample;
+    public static String[] validEmail = new String[] { "a@gmail.com", "ab@yahoo.com", "abc@hotmail.com" };
+    public static String[] invalidEmail = new String[] { "@gmail.com", "ab@gmail.", "@#abc@gmail.com" };
+
+    public static void main(String args[]) {
+        emailExample = new EmailExample();
+        for (String email : validEmail) {
+            boolean isvalid = emailExample.validate(email);
+            System.out.println("Email is " + email +" is valid: "+ isvalid);
+        }
+        for (String email : invalidEmail) {
+            boolean isvalid = emailExample.validate(email);
+            System.out.println("Email is " + email +" is valid: "+ isvalid);
+        }
+    }
+}
